5a6e2f1db4c4177e109728ce2bfdf3ff789d3fdf
DHWARAKESH SRINIVASAN <dhwarakesh99@gmail.com>
2025-03-07 01:11:01 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Jenkins CI/CD Pipeline Project of Dhwarakesh! (Updated code)"
+    return "Jenkins CI/CD Pipeline Project of Dhwarakesh! (Updated)"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
